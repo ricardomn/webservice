3768be2bd5a8eccad3788f2196749fcce1838b1c
Ricardo Mateus <ricardomateus23@hotmail.com>
2017-05-20 23:58:30 -0300

    fizanlizando testes de integracao

--- src/test/java/br/com/entity/ProfissionalTest.java
+++ src/test/java/br/com/entity/ProfissionalTest.java
@@ -1,11 +1,12 @@
 package br.com.entity;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
 import org.junit.Test;
+
+import builders.CriadorDeProfissional;
 
 public class ProfissionalTest extends AbstractDbUnitTest{
 
@@ -24,4 +25,62 @@ public class ProfissionalTest extends AbstractDbUnitTest{
 		assertEquals("PROFISSIONAL 2", lista.get(1).getNome());
 	}
 	
+	@Test
+	public void deveSerPossivelDeletarUmProfissionalPassandoPassandoComoParametroOMesmo() throws Exception {
+		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 1", "12345678910");
+		dao.delete(profissional);
+		assertEquals(3, dao.listAll().size());
+	}
+	
+	@Test
+	public void deveSerPossivelCarregarUmProfissionalQuandoSerPassarId() throws Exception {
+		Profissional profissional = dao.findById(1L);
+		assertNotNull(profissional);
+		assertEquals("PROFISSIONAL 1", profissional.getNome());
+	}
+	
+	@Test
+	public void deveSerPossivelCarregarUmProfissionalAoPassarUmCpf() throws Exception {
+		Profissional profissional = dao.findByCpf("12345678910");
+		assertNotNull(profissional);
+		assertEquals("PROFISSIONAL 1", profissional.getNome());
+	}
+	
+	@Test(expected=Exception.class)
+	public void deverSerPossivelLancarExceptionAoTentarGravarProfissionalSemCpf() throws Exception {
+		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 4", null);
+		dao.salvar(profissional);
+	}
+	
+	@Test
+	public void deveSerPossivelAtualizarUmProfissional() throws Exception {
+		Profissional profissional = dao.findByCpf("12345678910");
+		profissional.setNome("PROFISSIONAL ATUALIADO");
+		dao.update(profissional);
+		assertEquals("PROFISSIONAL ATUALIADO", dao.findByCpf("12345678910").getNome());
+	}
+	
+	@Test
+	public void deveSerPossivelRetornarNullQuandoNaoEncontrarUmProfissionalComOCpf() throws Exception {
+		Profissional profissional = dao.findByCpf("12358354");
+		assertNull(profissional);
+	}
+	
+	@Test
+	public void deveSerPossivelRetornarNullQuandoNaoEncontrarUmProfissionalPassandoIdInexistente() throws Exception {
+		Profissional profissional = dao.findById(1234L);
+		assertNull(profissional);
+	}
+	
+	@Test(expected=Exception.class)
+	public void deveSerPossivelRetornarExceptionQuandoTentarDeletarProfissionalInexistente() throws Exception {
+		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 4", "452245");
+		dao.delete(profissional);
+	}
+	
+	@Test(expected=Exception.class)
+	public void deveSerPossivelLancarUmaExceptionQuandoTentarCadastrarProfissionalComCpfMaiorQue11Caracteres() throws Exception {
+		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 4", "123456789102");
+		dao.salvar(profissional);
+	}
 }
