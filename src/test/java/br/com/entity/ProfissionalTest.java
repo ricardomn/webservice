60d1925faa1ad65d4da07ee780509f513e14b1ee
Ricardo Mateus <ricardomateus23@hotmail.com>
2017-05-21 16:45:57 -0300

    rename profissionalTest para ProfissionalDaoTest

--- src/test/java/br/com/entity/ProfissionalTest.java
+++ src/test/java/br/com/entity/ProfissionalTest.java
@@ -2,85 +2,36 @@ package br.com.entity;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
-
 import org.junit.Test;
 
 import builders.CriadorDeProfissional;
 
-public class ProfissionalTest extends AbstractDbUnitTest{
+public class ProfissionalTest {
 
 	@Test
-	public void deveSerPossivelEncontrarProfissionalQuandoPassarParametroId() {
-		
-		Profissional profissional = dao.findById(1L);
-		assertNotNull(profissional);
-		assertEquals("PROFISSIONAL 1", profissional.getNome());
-	}
-
-	@Test
-	public void deveSerPossivelRetonarUmaListaQuandoExecutarOMetodoListAll() throws Exception {
-		List<Profissional> lista = dao.listAll();
-		assertEquals(4, lista.size());
-		assertEquals("PROFISSIONAL 2", lista.get(1).getNome());
+	public void naoDeveSerPossivelProfissionaisComCpfMenorQueOnzeCaracteres() {
+		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 1", "1234");
+		assertEquals(false, profissional.isCpfValido());
 	}
 	
 	@Test
-	public void deveSerPossivelDeletarUmProfissionalPassandoPassandoComoParametroOMesmo() throws Exception {
+	public void naoDeveSerPossivelProfissionaisComCpfMaiorQue11Caracteres() throws Exception {
+		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 1", "123456789102");
+		assertEquals(false, profissional.isCpfValido());
+	}
+
+	@Test
+	public void deveSerPossivelProfissionaisComCpfComCpfComOnzeCaracteress() throws Exception {
 		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 1", "12345678910");
-		dao.delete(profissional);
-		assertEquals(3, dao.listAll().size());
+		assertEquals(true, profissional.isCpfValido());
 	}
 	
 	@Test
-	public void deveSerPossivelCarregarUmProfissionalQuandoSerPassarId() throws Exception {
-		Profissional profissional = dao.findById(1L);
-		assertNotNull(profissional);
-		assertEquals("PROFISSIONAL 1", profissional.getNome());
-	}
-	
-	@Test
-	public void deveSerPossivelCarregarUmProfissionalAoPassarUmCpf() throws Exception {
-		Profissional profissional = dao.findByCpf("12345678910");
-		assertNotNull(profissional);
-		assertEquals("PROFISSIONAL 1", profissional.getNome());
-	}
-	
-	@Test(expected=Exception.class)
-	public void deverSerPossivelLancarExceptionAoTentarGravarProfissionalSemCpf() throws Exception {
-		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 4", null);
-		dao.salvar(profissional);
-	}
-	
-	@Test
-	public void deveSerPossivelAtualizarUmProfissional() throws Exception {
-		Profissional profissional = dao.findByCpf("12345678910");
-		profissional.setNome("PROFISSIONAL ATUALIADO");
-		dao.update(profissional);
-		assertEquals("PROFISSIONAL ATUALIADO", dao.findByCpf("12345678910").getNome());
-	}
-	
-	@Test
-	public void deveSerPossivelRetornarNullQuandoNaoEncontrarUmProfissionalComOCpf() throws Exception {
-		Profissional profissional = dao.findByCpf("12358354");
-		assertNull(profissional);
-	}
-	
-	@Test
-	public void deveSerPossivelRetornarNullQuandoNaoEncontrarUmProfissionalPassandoIdInexistente() throws Exception {
-		Profissional profissional = dao.findById(1234L);
-		assertNull(profissional);
-	}
-	
-	@Test(expected=Exception.class)
-	public void deveSerPossivelRetornarExceptionQuandoTentarDeletarProfissionalInexistente() throws Exception {
-		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 4", "452245");
-		dao.delete(profissional);
-	}
-	
-	@Test(expected=Exception.class)
-	public void deveSerPossivelLancarUmaExceptionQuandoTentarCadastrarProfissionalComCpfMaiorQue11Caracteres() throws Exception {
-		Profissional profissional = new CriadorDeProfissional().addProfissional("PROFISSIONAL 4", "123456789102");
-		dao.salvar(profissional);
+	public void naoDeveSerPossivelProfissionaisComCpfNulosOuVazio() throws Exception {
+		Profissional profissional1 = new CriadorDeProfissional().addProfissional("PROFISSIONAL 1", "");
+		assertEquals(false, profissional1.isCpfValido());
+		
+		Profissional profissional2 = new CriadorDeProfissional().addProfissional("PROFISSIONAL 1", null);
+		assertEquals(false, profissional2.isCpfValido());
 	}
 }
