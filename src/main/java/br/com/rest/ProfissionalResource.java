f401d4e96dcda1b62867b475f51d1c087873a1da
Ricardo Mateus <ricardomateus23@hotmail.com>
2017-05-28 20:33:44 -0300

    adicionado autenticacao por cabecalho modo Basic

--- src/main/java/br/com/rest/ProfissionalResource.java
+++ src/main/java/br/com/rest/ProfissionalResource.java
@@ -1,10 +1,14 @@
 package br.com.rest;
 
+
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.internal.util.Base64;
 
 import br.com.dao.ProfissionalDao;
 import br.com.entity.Profissional;
@@ -18,11 +22,36 @@ public class ProfissionalResource {
 	@GET
 	@Path("/{cpf}")
     @Produces(MediaType.APPLICATION_JSON)
-	public Profissional profissionalBy(@PathParam("cpf") String cpf){
+	public Object profissionalBy(@PathParam("cpf") String cpf, @HeaderParam("authorization") String authString){
+		if(!isUserAuthenticaded(authString))
+			return "{\"error\": \"Usuário não autentidado!\"}";
+		
 		dao = new ProfissionalDao(JPAUtil.getEntityManager());
 		Profissional profissional = dao.findByCpf(cpf);
 		JPAUtil.closeEntityManagerFactory();
+		
+		
 		return profissional;
 	}
 
+	private boolean isUserAuthenticaded(String authString) {
+		
+		if(authString == null)
+			return false;
+		
+		String decodedAuth = "";
+		String[] authParts = authString.split("\\s+");
+		String authInfor = authParts[1];
+		byte[] bytes = null;
+		try {
+			bytes = Base64.decode(authInfor.getBytes());
+		} catch (Exception e) {
+			return false;
+		}
+		decodedAuth = new  String(bytes);
+		System.out.println(decodedAuth);
+		return true;
+	}
+	
+	
 }
