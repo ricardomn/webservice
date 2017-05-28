afa983987892c2326b6fb7e954b7f0a1ea7a6e9c
Ricardo Mateus <ricardomateus23@hotmail.com>
2017-05-28 20:35:19 -0300

    testanto webservice com autenticacao

--- src/main/java/br/com/main/ProfissionalTeste.java
+++ src/main/java/br/com/main/ProfissionalTeste.java
@@ -1,17 +1,35 @@
 package br.com.main;
 
-import javax.persistence.EntityManager;
 
-import br.com.dao.ProfissionalDao;
-import br.com.util.JPAUtil;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.internal.util.Base64;
+
+import br.com.entity.Profissional;
 
 public class ProfissionalTeste {
 	public static void main(String[] args) {
-		EntityManager entityManager = JPAUtil.getEntityManager();
-		
-		ProfissionalDao dao = new ProfissionalDao(entityManager);
-		System.out.println((dao.listAll()));
-		JPAUtil.closeEntityManagerFactory();
-		
+		String url = "http://localhost:8080/webservice/profissional/";
+		String name = "usuarioteste";
+		String password = "123456";
+		String authString = name + ":" + password;
+		String authStringEnc = new String(Base64.encode(authString.getBytes()));
+		System.out.println("Base 64 encoded auth String: " + authStringEnc);
+		Client restClient = ClientBuilder.newClient();
+		WebTarget webResource = restClient.target(url+"22222222222");
+		Response resp = webResource.request("application/json")
+							.header("Authorization", "Basic " + authStringEnc)
+							.get(Response.class);
+		if(resp.getStatus()!=200) {
+			System.out.println("Sem conexão com servidor");
+			
+		}else{
+			Profissional profissional = (Profissional) resp.readEntity(Object.class);
+			System.out.println(profissional);
+		}
 	}
 }
